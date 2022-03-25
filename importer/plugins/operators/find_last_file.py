1ba1297da69b9db75ce8e5bb3efd79cd4e61e47c
Léni Marvaud <24732919+lmarvaud@users.noreply.github.com>
2022-03-25 21:29:11 +0100

    type: 🏷️  create a Context type

--- importer/plugins/operators/find_last_file.py
+++ importer/plugins/operators/find_last_file.py
@@ -1,8 +1,9 @@
 import glob
 import os
 from operator import itemgetter
-from typing import Dict, Iterable, Iterator, Tuple, Optional
+from typing import Iterable, Iterator, Tuple, Optional
 
+from common.types import Context
 from pendulum import DateTime, UTC
 
 from airflow.hooks.filesystem import FSHook
@@ -11,8 +12,6 @@ from airflow.exceptions import AirflowSkipException
 
 TimeIntervale = Tuple[DateTime, DateTime]
 
-
-Context = Dict
 
 class FindLastFileOperator(BaseOperator):
     """
