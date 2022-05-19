9c230f667681bfe9b8b0b9ba5593246a2a47655e
Léni Marvaud <24732919+lmarvaud@users.noreply.github.com>
2022-05-19 14:22:27 +0200

    👷 launch deployment when the openvpn is up

--- .execute.sh
+++ .execute.sh
@@ -2,8 +2,8 @@
 
 usage() {
 	echo "Usage:";
-	echo "- $0 deploy [path to private ssh key] [IP] [env vars to write to .env] : lance une mise en production";
-	echo "- $0 test [path to private ssh key] [URL to test] : lance une sequence de tests";
+	echo "- $0 connect_vpn [openvpn config] [timeout in seconds (default: 30)] : connecte le serveur au vpn";
+	echo "- $0 deploy [IP] [path to private ssh key] [env vars to write to .env] : lance une mise en production";
 	echo "- $0 restore [docker archive] : lance une restauration de docker";
 }
 
@@ -20,46 +20,31 @@ log_on_error() {
 
 connect_openvpn() {
   local VPN_CONFIG="$1";
+  local TIMEOUT="${2:-30}"
 
-  ovpn=$(mktemp --suffix=.ovpn)
-  echo "$VPN_CONFIG" > ${ovpn}
-  sudo openvpn --dev tun0 --daemon --config ${ovpn}
-  log_on_error "$?" "Openvpn connection failed"
-}
+  local openvpn=$(mktemp --tmpdir="$RUNNER_TEMP" --suffix=.ovpn);
+  local watch_file=$(mktemp --tmpdir="$RUNNER_TEMP" --dry-run);
+  local bash_path=$(command -v bash);
 
-ping_with_timeout() {
-  local IP="$1"
-  local TIMEOUT="${2:-10}"
+  echo "$VPN_CONFIG" > ${openvpn};
 
-  timeout ${TIMEOUT} bash -c "until ping -c1 ${IP}; do :; done"
-  log_on_error "$?" "IP not accessible after ${TIMEOUT} seconds"
-}
+  sudo openvpn \
+    --dev tun0 \
+    --config ${openvpn} \
+    --script-security 2 \
+    --daemon \
+    --up "'$bash_path' -c 'touch $watch_file'";
 
-connect_openvpn_and_check_connection() {
-  local VPN_CONFIG="$1";
-  local IP="$2"
-  local TIMEOUT="${3:-10}"
-
-  connect_openvpn "${VPN_CONFIG}" || return $?
-  ping_with_timeout "${IP}" "${TIMEOUT}" || return $?
+  timeout "${TIMEOUT}" bash -c "until [ -e \"$watch_file\" ]; do sleep 1; done"
+  log_on_error "$?" "VPN not up after ${TIMEOUT} seconds"
 }
 
 livraison() {
 	local IP="$1";
-	local ARG="$2";
+	local IDENTITY_FILE="$2";
 	local ENV="$3";
-  local VPN_CONFIG="$4";
 
-  echo ${IP:0:3}
-  echo ${#VPN_CONFIG}
-  echo ${#ENV}
-
-  if [ "$VPN_CONFIG" != "" ]
-  then
-    connect_openvpn_and_check_connection "${VPN_CONFIG}" "${IP}" \
-      || return $?
-  fi
-	[ "$ARG" != "" ] && RSA="-i $ARG" || RSA="";
+	[ "$IDENTITY_FILE" != "" ] && RSA="-i $IDENTITY_FILE" || RSA="";
 	read -r -d "" SCRIPT <<EOF
 	  if [[ ! -e /home/docker/importer ]]
 	  then
@@ -80,21 +65,24 @@ EOF
 if [ -f .env ]; then source .env; fi
 
 CMD=$1;
-ARG="$2";
 
 case $CMD in
 	deploy)
-		IP="$3";
+		IP="$2";
+    IDENTITY_FILE="$3";
 		ENV="$4";
-		VPN_CONFIG="$5";
-		livraison "$IP" "$ARG" "$ENV" "$VPN_CONFIG";
+
+		livraison "$IP" "$IDENTITY_FILE" "$ENV";
 		;;
-	test)
-		DOMAIN="$3";
-		curl -s https://$DOMAIN | grep -i "logo" >/dev/null && echo "Test Ok" || echo "Test Ko";
-		;;
+  connect_openvpn)
+    VPN_CONFIG="$2";
+
+    connect_openvpn "$VPN_CONFIG"
+    ;;
 	restore)
-		if [ "$ARG" = "" ];
+	  ARCHIVE_NAME="$2"
+
+		if [ "$ARCHIVE_NAME" = "" ];
 		then
 			echo "### Liste des archives";
 			ls -1 backups/home/backups | grep -E ".*\.bz2";
@@ -102,7 +90,7 @@ case $CMD in
 			echo "Essayer:";
 			echo "$0 restore <fichier>";
 		else
-			tar -jxvf backups/home/backups/$ARG;
+			tar -jxvf backups/home/backups/$ARCHIVE_NAME;
 		fi
 		;;
 	*)
