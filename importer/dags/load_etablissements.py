413a5c19d0f225f1ae23eff36b17870ba7a3c8e2
Léni Marvaud <24732919+lmarvaud@users.noreply.github.com>
2022-05-24 16:47:04 +0200

    ♻️🎨 improve code structure of the DAG
    Add groups to increase readability of the graph

--- importer/dags/load_etablissements.py
+++ importer/dags/load_etablissements.py
@@ -1,5 +1,4 @@
 import datetime
-import os
 from pathlib import Path
 
 from airflow import DAG
@@ -7,6 +6,8 @@ from airflow.models.variable import Variable
 from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.sensors.filesystem import FileSensor
+from airflow.utils.task_group import TaskGroup
+from airflow.utils.trigger_rule import TriggerRule
 
 from operators.extract_offices import ExtractOfficesOperator
 from operators.find_last_file import FindLastFileOperator
@@ -30,33 +31,41 @@ with DAG("load-etablissements-2022-03",
          catchup=False,
          schedule_interval="@daily") as dag:
     start_task = DummyOperator(task_id="start")
-    sensor_task = FileSensor(  # type: ignore [no-untyped-call]
-        task_id="file_sensor_task",
-        retries=10,
-        retry_delay=1 if os.getenv('ENV_TYPE', default='production') == 'development' else 30,
-        filepath=filepath,
-    )
-    find_last_file = FindLastFileOperator(
-        task_id='find_last_tar',
-        filepath=filepath
-    )
-    untar_last_file = UntarOperator(
-        task_id='untar_last_tar',
-        source_path="{{ task_instance.xcom_pull(task_ids='find_last_tar', key='return_value') }}",
-        dest_path=working_tmp_dir
-    )
 
-    office_path_sensor_task = FileSensor(  # type: ignore [no-untyped-call]
-        task_id="office_path_sensor_task",
-        retries=0,
-        filepath=str(offices_path),
-    )
-    extract_offices = ExtractOfficesOperator(
-        task_id='extract_offices',
-        destination_table='etablissements_raw',
-        offices_filename=str(offices_path),
-    )
+    with TaskGroup("untar") as untar_group:
+        find_last_file = FindLastFileOperator(
+            task_id='find_last_tar',
+            filepath=filepath
+        )
+        untar_last_file = UntarOperator(
+            task_id='untar_last_tar',
+            source_path="{{ task_instance.xcom_pull(task_ids='" + find_last_file.task_id + "', key='return_value') }}",
+            dest_path=working_tmp_dir
+        )
 
+        find_last_file >> untar_last_file
+
+    with TaskGroup("offices") as offices_group:
+        office_path_sensor_task = FileSensor(  # type: ignore [no-untyped-call]
+            task_id="office_path_sensor_task",
+            retries=0,
+            filepath=str(offices_path),
+            doc="Check if the offices csv file is set"
+        )
+        extract_offices = ExtractOfficesOperator(
+            task_id='extract_offices',
+            destination_table='etablissements_raw',
+            offices_filename=str(offices_path),
+        )
+        office_path_sensor_task >> extract_offices
+
+    extract_scores_task = DummyOperator(task_id="extract_scores")
+    retrieve_geoloc_task = DummyOperator(task_id="retrieve_geoloc")
+
+    join = DummyOperator(
+        task_id='join_operations',
+        trigger_rule=TriggerRule.NONE_FAILED,
+    )
     rmdir = BashOperator(
         task_id='delete_result_directory',
         bash_command='rm -r ${WORKING_TMP_DIR}',
@@ -64,12 +73,10 @@ with DAG("load-etablissements-2022-03",
     )
     end_task = DummyOperator(task_id="end")
 
-start_task \
->> sensor_task \
->> find_last_file \
->> untar_last_file \
->> office_path_sensor_task \
->> extract_offices \
- \
->> rmdir \
->> end_task
+start_task >> untar_group
+
+untar_group >> offices_group >> join
+untar_group >> extract_scores_task >> join
+untar_group >> retrieve_geoloc_task >> join
+
+join >> rmdir >> end_task
