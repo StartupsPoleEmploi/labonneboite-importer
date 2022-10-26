b01b440c4b8e751cfa0c7a791b0a819470ed6a25
sylvain.touret <sylvain.touret@delia.tech>
2022-10-26 10:27:09 +0200

    added init tests

--- docker/entrypoints/test.sh
+++ docker/entrypoints/test.sh
@@ -13,6 +13,29 @@ set -e
 # init tests
 pip install -r <(poetry export --only dev --format requirements.txt)
 
+airflow variables import ${AIRFLOW_HOME}/settings/default.json;
+airflow variables import ${AIRFLOW_HOME}/settings/docker.json;
+
+airflow connections list --conn-id fs_default | grep fs_default > /dev/null \
+  || airflow connections add fs_default --conn-type fs
+
+airflow connections list --conn-id mysql_importer | grep mysql_importer > /dev/null \
+  && airflow connections delete mysql_importer
+airflow connections add mysql_importer \
+    --conn-host ${IMPORTER_MYSQL_HOST:-importer-mysql} \
+    --conn-login ${IMPORTER_MYSQL_LOGIN:-importer} \
+    --conn-password ${IMPORTER_MYSQL_PASSWORD:-importer} \
+    --conn-port ${IMPORTER_MYSQL_PORT:-3306} \
+    --conn-schema importer \
+    --conn-type mysql
+
+airflow connections list --conn-id http_address | grep http_address > /dev/null \
+  || airflow connections add http_address --conn-uri https://api-adresse.data.gouv.fr/
+
+alembic -c ${AIRFLOW_HOME}/settings/alembic.ini upgrade head
+
+
+
 # run the tests
 # -- lint
 if ! flake8 --output-file flake8.txt
