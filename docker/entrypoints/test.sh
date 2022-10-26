85440569a2f1f3b9bc215aa74a49b48bed083a73
sylvain.touret <sylvain.touret@delia.tech>
2022-10-26 09:18:58 +0200

    added the docker-compose.testing

---
+++ docker/entrypoints/test.sh
@@ -0,0 +1,48 @@
+# /bin/bash
+
+testReturn=0
+
+failed() {
+    testReturn=1
+    echo "FAILED $1"
+}
+
+set -e
+
+# init tests
+pip install -r <(poetry export --only dev --format requirements.txt)
+
+# run the tests
+# -- lint
+if ! flake8 --output-file flake8.txt
+then
+    failed "flake";
+    flake8_junit flake8.txt flake8.xml && echo;
+    rm flake8.txt;
+fi
+
+# -- type checking
+# if ! mypy --junit-xml ./mypy.xml .
+# then
+#     failed "mypy";
+# fi
+
+# -- unit test & coverage
+# -- api
+if pytest --junitxml=pytest.xml --cov
+then
+    coverage xml
+else
+    failed "pytest"
+fi
+
+
+# prepare test results
+echo "Moving test results file..."
+mkdir -p testResults
+chown -R "${AIRFLOW_UID}:0" ./testResults
+mv *.xml  ./testResults
+
+echo "Done"
+
+exit $testReturn
